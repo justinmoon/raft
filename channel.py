f05b3a2022b02d7a05ae0be85578d47a6088c301
Justin Moon <mail@justinmoon.com>
2019-03-04 14:47:02 -0600

    Serialization prototype

--- channel.py
+++ channel.py
@@ -1,13 +1,16 @@
 import socket
 import sys
+import pickle
+import json
 import time
 
 
 class Channel:
 
-    def __init__(self):
+    def __init__(self, serialization=None):
         self.sock = None
         self.address = None
+        self.serialization = serialization
 
     def connect(self, address):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,6 +23,16 @@ class Channel:
         self.sock, self.address = sock.accept()
 
     def send(self, msg):
+        if self.serialization == 'pickle':
+            msg = pickle.dumps(msg)
+        if self.serialization == 'json':
+            msg = json.dumps(msg.decode('utf-8')).encode('utf-8')
+        size = len(msg)
+        sizemsg = size.to_bytes(4, 'big')
+        self.sock.sendall(sizemsg)
+        self.sock.sendall(msg)
+
+    def send_raw(self, msg):
         size = len(msg)
         sizemsg = size.to_bytes(4, 'big')
         self.sock.sendall(sizemsg)
@@ -37,7 +50,12 @@ class Channel:
     def recv(self):
         sizemsg = self.sock.recv(4)
         size = int.from_bytes(sizemsg, 'big')
-        return self.recv_exactly(size)
+        msg = self.recv_exactly(size)
+        if self.serialization == 'pickle':
+            msg = pickle.loads(msg)
+        if self.serialization == 'json':
+            msg = json.loads(msg.decode('utf-8'))
+        return msg
 
 
 def client(address):
@@ -46,27 +64,62 @@ def client(address):
     c.send(b'hello')
 
     m = c.recv()
-    print("client received:", m)
+    print(f"client received {len(m)} bytes")
 
 
-def server(address):
-    c = Channel()
+def server(address, serialization=None):
+    c = Channel(serialization)
     c.accept(address)
 
     m = c.recv()
-    print("server received:", m)
+    print(f"server received {len(m)}")
 
     c.send(b'good day')
+
+
+def serialization_test(port):
+    # you need to manually run these three respective servers ...
+    # TODO: spawn processes for this so you don't need so many terminal window
+    address = ('localhost', port)
+    pickle_address = ('localhost', port + 1)
+    json_address = ('localhost', port + 2)
+
+    start = time.time()
+    jc = Channel()
+    jc.connect(address)
+    jc.send(b'\x00' * 100_000)
+    jc.recv()
+    end = time.time()
+    print(f"raw took {end - start} seconds")
+
+    start = time.time()
+    pc = Channel('pickle')
+    pc.connect(pickle_address)
+    pc.send(b'\x00' * 100_000)
+    pc.recv()
+    end = time.time()
+    print(f"pickle took {end - start} seconds")
+
+    start = time.time()
+    jc = Channel('json')
+    jc.connect(json_address)
+    jc.send(b'\x00' * 100_000)
+    jc.recv()
+    end = time.time()
+    print(f"json took {end - start} seconds")
 
 
 if __name__ == '__main__':
     args = sys.argv
     command = args[1]
     port = int(args[2])
+    serialization = args[3]
     address = ('localhost', port)
     if command == 'client':
         client(address)
+    elif command == 'test':
+        serialization_test(port)
     elif command == 'server':
-        server(address)
+        server(address, serialization)
     else:
-        print('python channel.py <client|server>')
+        print('python channel.py <test|client|server> <address> <raw|json|pickle>')

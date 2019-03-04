cd5391dabe7b9af9af89f98e6c356efd90e76da8
Justin Moon <mail@justinmoon.com>
2019-03-04 10:46:01 -0600

    SQLite demo

--- sample.py
+++ sample.py
@@ -1,4 +1,5 @@
 import logging
+import sqlite3
 
 
 class FileHandler(logging.StreamHandler):
@@ -16,9 +17,58 @@ class FileHandler(logging.StreamHandler):
             f.write(msg + '\n')
 
 
-def main():
+class SQLiteHandler(logging.StreamHandler):
+
+    def __init__(self):
+        logging.StreamHandler.__init__(self)
+        fmt = '%(levelname)-8s: %(message)s'
+        formatter = logging.Formatter(fmt)
+        self.setFormatter(formatter)
+
+    def emit(self, record):
+        print("record:", record)
+        msg = self.format(record)
+        print("msg:", msg)
+        with open("log.txt", "a") as f:
+            f.write(msg + '\n')
+
+
+def sqlite_demo():
+    conn = sqlite3.connect('log.db')
+
+    def create_table():
+        conn.execute('''
+        CREATE TABLE messages (level text, timestamp text, message text)
+        ''')
+
+    def record(message):
+        q = '''
+        INSERT INTO
+            messages
+        VALUES
+            ("INFO", "today", "OMG")
+        '''
+        conn.execute(q)
+
+    def query():
+        return conn.execute("SELECT * FROM messages").fetchall()
+
+    try:
+        create_table()
+    except:
+        pass
+
+    record("FIXME")
+    record("FIXME")
+    conn.commit()
+    print(query())
+
+
+def file_handler_demo():
     log = logging.getLogger(__name__)
     log.addHandler(FileHandler())
     log.error("OMG")
 
-main()
+
+if __name__ == '__main__':
+    sqlite_demo()

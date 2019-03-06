3f17406da35bae86deacdc80fa471401c9404df6
Justin Moon <mail@justinmoon.com>
2019-03-05 22:46:59 -0600

    Better demo with election & heartbeat

--- raft/state.py
+++ raft/state.py
@@ -8,49 +8,110 @@ import logging
 
 logging.basicConfig(level="INFO", format='%(threadName)-6s | %(message)s')
 
+queues = [queue.Queue() for _ in range(5)]
 
 class Log:
     pass
 
 
 class Controller:
-    pass
+
+    def __init__(self, node_id):
+        self.node_id = node_id
+        self.timer = None
+        # TODO: should I instantiate the RaftMachine here?
+
+    def request_vote(self):
+        # FIXME: there should be a timer associated with this
+        msg = {
+            "command": "request_vote",
+            "node_id": self.node_id,
+        }
+        send_msg(self.node_id, msg)
+
+    def vote(self, candidate_id, term, vote_granted):
+        msg = {
+            "command": "vote",
+            "term": term,
+            "vote_granted": vote_granted,
+            "node_id": self.node_id,
+        }
+        queues[candidate_id].put(msg)
+        print(f'Node {self.node_id} voted for {candidate_id}?: {vote_granted}')
+
+    def send_heartbeat(self):
+        print(f'Node {self.node_id} sent heartbeat')
+
+        # Send heartbeat
+        msg = {
+            'command': 'append_entries',
+            'node_id': self.node_id,
+        }
+        send_msg(self.node_id, msg)
+
+        # Schedule next heartbeat
+        threading.Timer(.5, self.send_heartbeat).start()
+
+    def reset_timer(self):
+        q = queues[self.node_id]
+        if self.timer:  # FIXME
+            self.timer.cancel()
+        self.timer = threading.Timer(1, timeout, (q,))
+        self.timer.start()
+
+def timeout(q):
+    msg = {
+        'command': 'timeout',
+    }
+    q.put(msg)
 
 
-def run(q):
+# FIXME dumb function name. 
+# Sounds like it would send to node_id, instead sends to everyone.
+# Controller.broadcast would be a better name
+def send_msg(node_id, msg):
+    for i in range(5):
+        if i != node_id:
+            queues[i].put(msg)
 
-    # where's the controller?
-    # who writes to the queue?
-    # - presumably there are threads that do this
-    # - how can a timer interrupt this?
 
-    # q = queue.Queue()
-    raft = RaftMachine()
+def target(node_id):
+    q = queues[node_id]
+    raft_machine = RaftMachine(node_id)
+    timer = None
 
-    def alarm_handler(signum, frame):
-        # FIXME
-        if signum == signal.SIGALRM:
-            print(f'calling for election from {threading.currentThread()}')
-
-    # Set alarms for timeouts
-    signal.signal(signal.SIGALRM, alarm_handler)
+    # node 0 starts us off FIXME 
+    if node_id == 0:
+        print("Node 0 requests vote")
+        raft_machine.controller.request_vote()
 
     while True:
-        print('waiting for messages')
-        msg_type, msg_data = q.get()
-        print('got message', msg_type, msg_data)
-        if msg_type == 'append_entries':
-            raft.handle_append_entries(msg_data)
-        elif msg_type == 'request_vote':
-            raft.handle_request_vote(msg_data)
+        # Get next message
+        msg = q.get()
+
+        # Handle the message
+        handler = getattr(raft_machine, f'handle_{msg["command"]}')
+        handler(msg)
+
+
+def main():
+    threads = []
+    for node_id in range(5):
+        thread = threading.Thread(target=target, args=(node_id,))
+        thread.start()
+        threads.append(thread)
 
 
 class RaftMachine:
 
-    def __init__(self):
+    def __init__(self, node_id):
+        self.node_id = node_id
         self.state = Follower
-        self.controller = Controller()
+        self.controller = Controller(node_id)
         self.log = Log()
+
+        # Elections
+        self.votes = [None] * 5
 
         # Mystery params from the papaer
         self.current_term = 0
@@ -60,19 +121,36 @@ class RaftMachine:
         self.next_index = []
         self.match_index = []
 
-    def handle_timeout(self):
+    def handle_timeout(self, msg):
         self.controller.request_vote()
 
     def handle_request_vote(self, msg):
+        self.votes[self.node_id] = True  # vote for themself
         self.current_term += 1
         vote_granted = True
-        return self.current_term, vote_granted
+        self.controller.vote(msg['node_id'], self.current_term, vote_granted)
+
+    def handle_vote(self, msg):
+        self.votes[msg['node_id']] = msg['vote_granted']
+
+        y = len([v for v in self.votes if v is True])
+        n = len([v for v in self.votes if v is False])
+
+        # Election won
+        if y > 2:
+            self.votes = [None] * 5
+            self.current_term += 1
+            self.controller.send_heartbeat()
+
+        # Election lost
+        if n > 2:
+            self.votes = [None] * 5
 
     def handle_append_entries(self, msg):
 
         # If it's from our last leader, defer timeout
-        if msg['leader_id'] == self.voted_for:
-            signal.alarm(1)
+        if msg['node_id'] == self.voted_for:
+            self.controller.reset_timer()
         else:
             success = False
             return self.current_term, success
@@ -100,22 +178,5 @@ class Candidate(BaseState):
     pass
 
 
-def test(q):
-    while True:
-        pause = random.random() * 2
-        msg_data = {
-            'leader_id': 0,
-        }
-        time.sleep(pause)
-        q.put(('append_entries', msg_data))
-
-
-
-def demo():
-    q = queue.Queue()
-    threading.Thread(target=test, args=(q,)).start()
-    run(q)
-
-
 if __name__ == '__main__':
-    demo()
+    main()
